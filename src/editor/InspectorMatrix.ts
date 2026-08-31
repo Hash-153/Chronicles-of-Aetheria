f8c6ca6fa91056747dfcc2d4227a89ff14c200ca
Aether Core Developer <dev@aetheria.internal>
2026-09-01 04:09:41 +0530

    feat(editor): Add component property reflection descriptors for level editor

---
+++ src/editor/InspectorMatrix.ts
@@ -0,0 +1,5 @@
+/**
+ * @file InspectorMatrix.ts
+ * @description Component property reflection descriptors for level editor.
+ */
+export const INSPECTOR_MATRIX = { Transform2D: ['x', 'y', 'rotation'], Attributes: ['health', 'mana'] };
